3ae580a7a65b655a6907ddc5ae0716e5ebe3b455
jain-aha <ahan@ahanjain.com>
2025-09-22 15:04:23 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
   const targetTextRef = useRef('');
 
   const typingPhrases = [
-    "a Software Engineer @ Darby",
+    "a Software Engineer @ Darby AI",
     "a CS & AI Major @ Northeastern University",
     "a Pianist, Drummer, Guitarist and Vocalist",
     "a Backend Developer",
